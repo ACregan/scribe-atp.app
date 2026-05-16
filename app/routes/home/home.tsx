be5e49f654df417dec1c4e9958ebdbd1f9c680ac
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-05-17 00:56:05 +0800

    Auth Test

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -7,11 +7,33 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export async function loader({ request }: Route.LoaderArgs) {
+  const cookieHeader = request.headers.get("cookie");
+  const cookies = new Map(
+    (cookieHeader || "")
+      .split(";")
+      .map((c) => {
+        const [key, value] = c.trim().split("=");
+        return [key, value];
+      })
+  );
+
+  const isAuthenticated = !!cookies.get("bsky_auth");
+
+  return { isAuthenticated };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { isAuthenticated } = loaderData;
+
   return (
     <>
       <h1>Home Page</h1>
-      <p>User is NOT Authenticated</p>
+      <p>
+        {isAuthenticated
+          ? "Welcome to Scribe"
+          : "User is NOT Authenticated"}
+      </p>
     </>
   );
 }
