7d99e65478bfc5c4dc2a7de5d4b3f6052b24a0db
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-05-17 14:16:12 +0800

    Revert this slop

--- app/routes.ts
+++ app/routes.ts
@@ -8,8 +8,6 @@ import {
 export default [
   layout("./layout/core/core.tsx", [
     index("./routes/home/home.tsx"),
-    route("login", "./routes/login/login.tsx", [
-      route("callback", "./routes/login/callback.tsx"),
-    ]),
+    route("login", "./routes/login/login.tsx"),
   ]),
 ] satisfies RouteConfig;
