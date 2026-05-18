feb45afa5058a561e3dfd0ce086e9a10a009eed1
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-05-18 16:25:43 +0800

    Added tooltip component for use in sidemenu later

---
+++ app/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,104 @@
+import React, {
+  Children,
+  cloneElement,
+  isValidElement,
+  type ReactNode,
+  type CSSProperties,
+} from "react";
+import classNames from "classnames/bind";
+
+import styles from "./Tooltip.module.css";
+
+const cx = classNames.bind(styles);
+
+type AnchorPositionTypes =
+  | "top"
+  | "center"
+  | "bottom"
+  | "left"
+  | "right"
+  | "top left"
+  | "top right"
+  | "left top"
+  | "right top"
+  | "bottom left"
+  | "bottom right"
+  | "left bottom"
+  | "right bottom";
+
+interface TooltipProps {
+  children: ReactNode;
+  anchorName: string;
+  anchorContent: ReactNode;
+  anchorPosition: AnchorPositionTypes;
+  zIndex?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+  children,
+  anchorName,
+  anchorContent,
+  anchorPosition,
+  zIndex = 500,
+}) => {
+  const sanitisedAnchorName = anchorName.replaceAll(" ", "-").toLowerCase();
+  return (
+    <>
+      {Children.map(children, (child) => {
+        if (
+          isValidElement<{ style?: CSSProperties; className?: string }>(child)
+        ) {
+          return cloneElement(child, {
+            style: {
+              ...child.props.style,
+              anchorName: `--${sanitisedAnchorName}`,
+            },
+            className: `${child.props.className} ${styles.anchoredElement}`,
+          });
+        }
+        return child;
+      })}
+      <div
+        className={styles.tooltipElement}
+        style={{
+          positionAnchor: `--${sanitisedAnchorName}`,
+          positionArea: anchorPosition,
+          zIndex: zIndex,
+        }}
+      >
+        {anchorContent}
+      </div>
+    </>
+  );
+};
+
+interface TooltipBubbleProps {
+  children: ReactNode;
+  pointerLocation: AnchorPositionTypes;
+}
+
+export const TooltipBubble: React.FC<TooltipBubbleProps> = ({
+  children,
+  pointerLocation,
+}) => {
+  const pointerClasses = cx({
+    top: pointerLocation === "top",
+    left: pointerLocation === "left",
+    bottom: pointerLocation === "bottom",
+    right: pointerLocation === "right",
+    topRight:
+      pointerLocation === "top right" || pointerLocation === "right top",
+    topLeft: pointerLocation === "top left" || pointerLocation === "left top",
+    bottomLeft:
+      pointerLocation === "bottom left" || pointerLocation === "left bottom",
+    bottomRight:
+      pointerLocation === "bottom right" || pointerLocation === "right bottom",
+  });
+  return (
+    <div className={`${styles.tooltipBubble} ${pointerClasses}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Tooltip;
