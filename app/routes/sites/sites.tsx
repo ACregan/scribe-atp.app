1449d558a0c776260e5c532df261a8e2902423e3
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-05-23 23:01:20 +0800

    Manage Sites page UI WIP

--- app/routes/sites/sites.tsx
+++ app/routes/sites/sites.tsx
@@ -1,4 +1,15 @@
+import {
+  PageContainer,
+  PageSection,
+} from "~/components/PageContainer/PageContainer";
 import type { Route } from "./+types/sites";
+import { Select } from "~/components/Select/Select";
+import { Input } from "~/components/Input/Input";
+import { Modal } from "~/components/Modal/Modal";
+import { useModal } from "~/components/Modal/useModal";
+import { Button } from "~/components/Button/Button";
+import { useState } from "react";
+import styles from "./sites.module.css";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,8 +28,66 @@ export function HydrateFallback() {
 export async function loader({ request }: Route.LoaderArgs) {
   const mockSiteData = [
     {
-      domain: "norobots.blog",
+      url: "norobots.blog",
       title: "NoRobots.blog",
+      urlPrefix: "blog",
+      ownerId: "did:ofTheOwner",
+      contributors: ["did:ofContributorOne", "did:ofContributorTwo"],
+      groups: [
+        {
+          slug: "creative-writing",
+          type: "group",
+          title: "Creative Writing",
+          children: [
+            {
+              slug: "llms-are-full-of-shit",
+              type: "article",
+            },
+          ],
+        },
+        {
+          slug: "blogging",
+          type: "group",
+          title: "Blog",
+          children: [
+            {
+              slug: "the-crows-of-shenton-way",
+              type: "article",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      url: "perpetualsummer.ltd",
+      title: "Perpetual Summer LTD",
+      urlPrefix: "articles",
+      ownerId: "did:ofTheOwner",
+      contributors: ["did:ofContributorOne", "did:ofContributorTwo"],
+      groups: [
+        {
+          slug: "tech",
+          type: "group",
+          title: "Technology",
+          children: [
+            {
+              slug: "some-article-slug",
+              type: "article",
+            },
+          ],
+        },
+        {
+          slug: "business",
+          type: "group",
+          title: "Business News",
+          children: [
+            {
+              slug: "some-other-article-slug",
+              type: "article",
+            },
+          ],
+        },
+      ],
     },
   ];
   return { sites: mockSiteData };
@@ -28,10 +97,75 @@ export default function Sites({ loaderData }: Route.ComponentProps) {
   const { sites } = loaderData;
   console.log("sites", sites);
 
+  const [selected, setSelected] = useState<string>("");
+  const addSiteModal = useModal();
+
+  const selectDropdownOptions = sites.map((site) => {
+    return {
+      value: site.url,
+      label: site.title,
+    };
+  });
+
   return (
-    <>
-      <h1>Sites</h1>
-      <p>Manage the websites you are writing for here</p>
-    </>
+    <PageContainer
+      title="Manage Sites"
+      topButtons={
+        <Button type="button" onClick={addSiteModal.open}>
+          Add New Site
+        </Button>
+      }
+    >
+      <PageSection>
+        <Select
+          name="site"
+          label="Site"
+          options={selectDropdownOptions}
+          // options={[
+          //   { value: "uid:NoRobotsId", label: "NoRobots.blog" },
+          //   { value: "uid:PerpetualSummerId", label: "PerpetualSummer.ltd" },
+          // ]}
+          value={selected}
+          onChange={setSelected}
+        />
+      </PageSection>
+
+      <Modal
+        isOpen={addSiteModal.isOpen}
+        onClose={addSiteModal.close}
+        title="Add New Site"
+        footer={
+          <div className={styles.modalFooter}>
+            <Button variant="secondary" onClick={addSiteModal.close}>
+              Cancel
+            </Button>
+            <Button type="submit" form="add-site-form">
+              Add Site
+            </Button>
+          </div>
+        }
+      >
+        <form id="add-site-form">
+          <Input
+            id="site-title"
+            name="title"
+            label="Title"
+            placeholder="My Blog"
+          />
+          <Input
+            id="site-url"
+            name="url"
+            label="URL"
+            placeholder="myblog.com"
+          />
+          <Input
+            id="site-urlPrefix"
+            name="urlPrefix"
+            label="URL prefix"
+            placeholder="blog"
+          />
+        </form>
+      </Modal>
+    </PageContainer>
   );
 }
